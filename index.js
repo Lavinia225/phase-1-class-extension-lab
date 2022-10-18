462e82fe7290de4c8df54d6db7cc332551e56daa
luna seifert <thepuddingdeity@gmail.com>
2022-10-18 17:36:32 -0500

    commit tree 70824081cf5fae59fbe24643b4a7a78a93918c67

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.sides = array
+    }
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce(accumulator, 0)
+
+        function accumulator(previous, current){
+            return previous + current
+        }
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if ((this.sides[0] + this.sides[1] > this.sides[2]) && (this.sides[0] + this.sides[2] > this.sides[1]) && (this.sides[1] + this.sides[2] > this.sides[0])){
+            return true
+        }
+        return false
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        return this.sides[0] === this.sides[1] && this.sides[0] === this.sides[2] && this.sides[0] == this.sides[3]
+    }
+
+    get area(){
+        return this.sides[0] * this.sides[1]
+    }
+}
